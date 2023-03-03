e444add3e5527cde89bd362a5c7ab6db5ced25ae
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-03-03 15:53:44 +0400

    Added CurrencyService interface

---
+++ src/main/java/com/sguProject/backendExchange/services/interfaces/CurrencyService.java
@@ -0,0 +1,16 @@
+package com.sguProject.backendExchange.services.interfaces;
+
+import com.sguProject.backendExchange.models.Currency;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CurrencyService {
+    void add(Currency currency);
+
+    List<Currency> getAllCurrencies();
+
+    Optional<Currency> getCurrencyByTicker(String ticker);
+
+    Optional<Currency> getCurrencyByName(String name);
+}
