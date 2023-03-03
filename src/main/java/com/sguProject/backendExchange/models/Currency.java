b696513026f0d315318e76947cd73850edb88988
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-03-03 15:53:21 +0400

    Added getters and setters, hashcode and equals contract implementation

--- src/main/java/com/sguProject/backendExchange/models/Currency.java
+++ src/main/java/com/sguProject/backendExchange/models/Currency.java
@@ -2,10 +2,11 @@ package com.sguProject.backendExchange.models;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
 
 @Entity
 @Table(name = "Currency")
-public class Currency {
+public class Currency { // None, BTC, ETH, BNB, USDT, XRP, LTC, BCH;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -19,4 +20,48 @@ public class Currency {
     @NotEmpty(message = "Name should not be empty")
     @Column(name = "name")
     private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTicker() {
+        return ticker;
+    }
+
+    public void setTicker(String ticker) {
+        this.ticker = ticker;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Currency currency = (Currency) o;
+
+        if (id != currency.id) return false;
+        if (!Objects.equals(ticker, currency.ticker)) return false;
+        return Objects.equals(name, currency.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (ticker != null ? ticker.hashCode() : 0);
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        return result;
+    }
 }
