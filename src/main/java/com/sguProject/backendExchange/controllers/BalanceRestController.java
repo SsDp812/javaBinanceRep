55fb88e92de338942b4b1d8eb29acba0e88fff22
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-03-03 17:03:44 +0400

    Refactoring rename BalanceController > BalanceRestController

--- src/main/java/com/sguProject/backendExchange/controllers/BalanceRestController.java
+++ src/main/java/com/sguProject/backendExchange/controllers/BalanceRestController.java
@@ -2,7 +2,6 @@ package com.sguProject.backendExchange.controllers;
 
 import com.sguProject.backendExchange.models.Balance;
 import com.sguProject.backendExchange.services.interfaces.BalanceService;
-import com.sguProject.backendExchange.services.interfaces.CurrencyService;
 import org.hibernate.cfg.NotYetImplementedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -10,16 +9,16 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-public class BalanceController {
+@CrossOrigin(origins = "http://127.0.0.1:5500")
+public class BalanceRestController {
 
     private final BalanceService balanceService;
 
     @Autowired
-    public BalanceController(BalanceService balanceService) {
+    public BalanceRestController(BalanceService balanceService) {
         this.balanceService = balanceService;
     }
 
-    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/buy")
     public void buy(@RequestParam("coin1") String buyableTicker,
                     @RequestParam("coin2") String salableTicker,
@@ -27,7 +26,6 @@ public class BalanceController {
         balanceService.buyCurrency(buyableTicker, salableTicker, number);
     }
 
-    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/sell")
     public void sell(@RequestParam("coin1") String salableTicker,
                      @RequestParam("coin2") String buyableTicker,
@@ -35,13 +33,11 @@ public class BalanceController {
         balanceService.sellCurrency(salableTicker, buyableTicker, number);
     }
 
-    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/getBalanceAll")
     public List<Balance> getAllBalances() {
         return balanceService.getAll();
     }
 
-    @CrossOrigin(origins = "http://127.0.0.1:5500")
     @GetMapping("/getBalance/{coin}")
     public double getBalance(@PathVariable String coin){
         throw new NotYetImplementedException(); // TODO: Добавить возвращение баланса текущего аккаунта
