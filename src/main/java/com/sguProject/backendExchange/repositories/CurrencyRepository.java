44ff0dc6a80cbd15b215611cdb0edab02a32e92b
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-03-04 19:36:04 +0400

    Added 2 methods findByTicker and findByName

--- src/main/java/com/sguProject/backendExchange/repositories/CurrencyRepository.java
+++ src/main/java/com/sguProject/backendExchange/repositories/CurrencyRepository.java
@@ -3,5 +3,10 @@ package com.sguProject.backendExchange.repositories;
 import com.sguProject.backendExchange.models.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CurrencyRepository extends JpaRepository<Currency, Integer> {
+    Optional<Currency> findByTicker(String ticker);
+
+    Optional<Currency> findByName(String name);
 }
