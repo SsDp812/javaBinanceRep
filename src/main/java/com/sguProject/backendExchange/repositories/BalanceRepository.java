442bf2e06aab22981fa1a4729d61c348cdd6fd32
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-03-18 17:52:53 +0400

    Added methods findAllByOwner and findByOwnerAndCurrency

--- src/main/java/com/sguProject/backendExchange/repositories/BalanceRepository.java
+++ src/main/java/com/sguProject/backendExchange/repositories/BalanceRepository.java
@@ -1,7 +1,15 @@
 package com.sguProject.backendExchange.repositories;
 
+import com.sguProject.backendExchange.models.Account;
 import com.sguProject.backendExchange.models.Balance;
+import com.sguProject.backendExchange.models.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BalanceRepository extends JpaRepository<Balance, Integer> {
+    List<Balance> findAllByOwner(Account owner);
+
+    Optional<Balance> findByOwnerAndCurrency(Account owner, Currency currency);
 }
