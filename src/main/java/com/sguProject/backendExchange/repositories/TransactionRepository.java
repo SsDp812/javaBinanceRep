f8ceb58201865c81c678987074da8201b83d37ed
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-05 19:46:36 +0400

    Added method findAllBySellerIdOrBuyerId

--- src/main/java/com/sguProject/backendExchange/repositories/TransactionRepository.java
+++ src/main/java/com/sguProject/backendExchange/repositories/TransactionRepository.java
@@ -3,5 +3,8 @@ package com.sguProject.backendExchange.repositories;
 import com.sguProject.backendExchange.models.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
+    List<Transaction> findAllBySellerIdOrBuyerId(int seller, int buyer);
 }
