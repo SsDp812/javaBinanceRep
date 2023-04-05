68b18684ab56a94e240e4b9700f02df99db8c186
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-05 19:40:35 +0400

    Account service now extends UserDetailsService

--- src/main/java/com/sguProject/backendExchange/services/interfaces/AccountService.java
+++ src/main/java/com/sguProject/backendExchange/services/interfaces/AccountService.java
@@ -1,10 +1,11 @@
 package com.sguProject.backendExchange.services.interfaces;
 
 import com.sguProject.backendExchange.models.Account;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 import java.util.Optional;
 
-public interface AccountService {
+public interface AccountService extends UserDetailsService {
     void create(Account account);
 
     Optional<Account> findById(int id);
