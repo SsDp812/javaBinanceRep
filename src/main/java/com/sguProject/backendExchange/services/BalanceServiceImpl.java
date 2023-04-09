74e195352318df9661c317c7467dff070b7bc8ee
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-09 22:45:04 +0400

    added methods withdraw and deposit

--- src/main/java/com/sguProject/backendExchange/services/BalanceServiceImpl.java
+++ src/main/java/com/sguProject/backendExchange/services/BalanceServiceImpl.java
@@ -3,15 +3,18 @@ package com.sguProject.backendExchange.services;
 import com.sguProject.backendExchange.models.Account;
 import com.sguProject.backendExchange.models.Balance;
 import com.sguProject.backendExchange.models.Currency;
-import com.sguProject.backendExchange.models.Transaction;
 import com.sguProject.backendExchange.repositories.BalanceRepository;
-import com.sguProject.backendExchange.services.interfaces.*;
+import com.sguProject.backendExchange.services.interfaces.AccountService;
+import com.sguProject.backendExchange.services.interfaces.BalanceService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.Set;
 
 @Service
 @Transactional(readOnly = true)
@@ -21,26 +24,19 @@ public class BalanceServiceImpl implements BalanceService {
 
     private final BalanceRepository balanceRepository;
 
-    private final TransactionService transactionService;
-
     private final AccountService accountService;
 
-    private final CourseService courseService;
-
     @Autowired
-    public BalanceServiceImpl(BalanceRepository balanceRepository, CurrencyService currencyService, TransactionService transactionService, AccountService accountService, CourseService courseService) {
+    public BalanceServiceImpl(BalanceRepository balanceRepository, AccountService accountService) {
         this.balanceRepository = balanceRepository;
-        this.transactionService = transactionService;
         this.accountService = accountService;
-        this.courseService = courseService;
     }
 
     @Transactional
     @Override
-    public void create(Account owner, Currency currency) {
+    public Balance create(Account owner, Currency currency) {
         Balance balance = new Balance(currency, owner, INITIAL_BALANCE_AMOUNT);
-
-        owner.addBalance(balance);
+        return balanceRepository.save(balance);
     }
 
     @Override
@@ -49,7 +45,7 @@ public class BalanceServiceImpl implements BalanceService {
     }
 
     @Override
-    public List<Balance> getAllByOwner(Account owner) {
+    public Set<Balance> getAllByOwner(Account owner) {
         return balanceRepository.findAllByOwner(owner);
     }
 
@@ -58,68 +54,25 @@ public class BalanceServiceImpl implements BalanceService {
         return balanceRepository.findByOwnerAndCurrency(owner, currency);
     }
 
-    @Transactional
+    @Transactional(isolation = Isolation.SERIALIZABLE)
     @Override
-    public void sellCurrency(Account owner, Currency salable, Currency buyable, double saleAmount) {
-        Balance salableBalance = balanceRepository.findByOwnerAndCurrency(owner, salable)
-                .orElseThrow(IllegalArgumentException::new);
-        Balance buyableBalance = balanceRepository.findByOwnerAndCurrency(owner, buyable)
-                .orElseThrow(IllegalArgumentException::new);
+    public void withdraw(Account account, Currency currency, double amount) {
+        Balance balance = balanceRepository.findByOwnerAndCurrency(account, currency)
+                .orElseThrow(NoSuchElementException::new);
 
-        double buyAmount = getBuyAmount(buyable, salable, saleAmount);
-
-        salableBalance.withdraw(saleAmount);
-
-        buyableBalance.deposit(buyAmount);
-
-        createTransaction(owner, accountService.getBankAccount(),
-                buyable, buyAmount, salable, saleAmount);
-    }
-
-    @Transactional
-    @Override
-    public void buyCurrency(Account owner, Currency buyable, Currency salable, double buyAmount) {
-        Balance salableBalance = balanceRepository.findByOwnerAndCurrency(owner, salable)
-                .orElseThrow(IllegalArgumentException::new);
-        Balance buyableBalance = balanceRepository.findByOwnerAndCurrency(owner, buyable)
-                .orElseThrow(IllegalArgumentException::new);
-
-        double requiredSaleAmount = getSaleAmount(buyable, salable, buyAmount);
-
-        if (requiredSaleAmount > salableBalance.getAmount()) {
-            throw new IllegalArgumentException("Salable balance amount less than needs for success trade");
+        if (amount > balance.getAmount()) {
+            throw new IllegalArgumentException("Balance amount less than needs for success trade");
         }
 
-        salableBalance.withdraw(requiredSaleAmount);
-
-        buyableBalance.deposit(buyAmount);
-
-        createTransaction(accountService.getBankAccount(), owner,
-                buyable, buyAmount, salable, requiredSaleAmount);
+        balance.withdraw(amount);
     }
 
-    private void createTransaction(Account seller, Account buyer,
-                                   Currency buyable, double buyAmount, Currency salable, double saleAmount) {
-        Transaction transaction = new Transaction();
-        transaction.setSeller(seller);
-        transaction.setBuyer(buyer);
-        transaction.setPurchasedCurrency(buyable);
-        transaction.setPurchasedAmount(buyAmount);
-        transaction.setSoldCurrency(salable);
-        transaction.setSoldAmount(saleAmount);
+    @Transactional(isolation = Isolation.SERIALIZABLE)
+    @Override
+    public void deposit(Account account, Currency currency, double amount) {
+        Balance balance = balanceRepository.findByOwnerAndCurrency(account, currency)
+                .orElseGet(() -> create(account, currency));
 
-        transactionService.create(transaction);
-    }
-
-    private double getBuyAmount(Currency buyable, Currency salable, double salableAmount) {
-        return salableAmount / getCourse(buyable, salable);
-    }
-
-    private double getSaleAmount(Currency buyable, Currency salable, double buyableAmount) {
-        return buyableAmount * getCourse(buyable, salable);
-    }
-
-    private double getCourse(Currency dividend, Currency divisor) {
-        return courseService.getCourse(dividend, divisor);
+        balance.deposit(amount);
     }
 }
