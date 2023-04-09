e039564e36da8d8185295efa063fa29f0799b02a
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-09 22:24:55 +0400

    disable csrf for "/api/**" path

--- src/main/java/com/sguProject/backendExchange/config/SecurityConfig.java
+++ src/main/java/com/sguProject/backendExchange/config/SecurityConfig.java
@@ -1,12 +1,25 @@
 package com.sguProject.backendExchange.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
 public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/api/**")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin();
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
