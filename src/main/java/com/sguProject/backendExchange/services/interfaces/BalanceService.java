8cbd555d5db20712be9f78d5e210591f8d1f29af
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-09 22:37:07 +0400

    method create now return Balance

--- src/main/java/com/sguProject/backendExchange/services/interfaces/BalanceService.java
+++ src/main/java/com/sguProject/backendExchange/services/interfaces/BalanceService.java
@@ -6,17 +6,18 @@ import com.sguProject.backendExchange.models.Currency;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface BalanceService {
-    void create(Account owner, Currency currency);
+    Balance create(Account owner, Currency currency);
 
     List<Balance> getAll();
 
-    List<Balance> getAllByOwner(Account owner);
+    Set<Balance> getAllByOwner(Account owner);
 
     Optional<Balance> findBy(Account owner, Currency currency);
 
-    void sellCurrency(Account owner, Currency salable, Currency buyable, double saleAmount);
+    void withdraw(Account account, Currency currency, double amount);
 
-    void buyCurrency(Account owner, Currency buyable, Currency salable, double buyAmount);
+    void deposit(Account account, Currency currency, double amount);
 }
