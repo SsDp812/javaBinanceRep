53f540f0726c2ad5aa392b5c7b5a09858f507776
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-09 22:34:59 +0400

    New implementation pf Transaction

--- src/main/java/com/sguProject/backendExchange/models/Transaction.java
+++ src/main/java/com/sguProject/backendExchange/models/Transaction.java
@@ -1,48 +1,40 @@
 package com.sguProject.backendExchange.models;
 
-import com.sun.istack.NotNull;
+import com.sguProject.backendExchange.util.enums.Operation;
 
 import javax.persistence.*;
-import javax.validation.constraints.Positive;
 import java.time.LocalDateTime;
+import java.util.Objects;
 
 @Entity
-@Table(name = "Transaction",
-        uniqueConstraints = { @UniqueConstraint(columnNames = { "created_at", "seller_id", "buyer_id" })})
+@Table(name = "Transaction")
 public class Transaction {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "id", updatable = false)
     private int id;
 
-    @NotNull
-    @Column(name = "created_at")
+    @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
     @ManyToOne
-    @JoinColumn(name = "seller_id", referencedColumnName = "id")
-    private Account seller;
+    @JoinColumn(name = "account_id", referencedColumnName = "id", nullable = false, updatable = false)
+    private Account account;
 
     @ManyToOne
-    @JoinColumn(name = "buyer_id", referencedColumnName = "id")
-    private Account buyer;
+    @JoinColumn(name = "currency_pair_id", referencedColumnName = "id", nullable = false, updatable = false)
+    private CurrencyPair currencyPair;
 
-    @ManyToOne
-    @JoinColumn(name = "purchased_ticker", referencedColumnName = "ticker")
-    private Currency purchasedCurrency;
+    @Enumerated(EnumType.ORDINAL)
+    @Column(name = "operation", nullable = false, updatable = false)
+    private Operation operation;
 
-    @Positive
-    @Column(name = "purchased_amount")
-    private double purchasedAmount;
+    @Column(name = "course", nullable = false, updatable = false)
+    private double course;
 
-    @ManyToOne
-    @JoinColumn(name = "sold_ticker", referencedColumnName = "ticker")
-    private Currency soldCurrency;
-
-    @Positive
-    @Column(name = "sold_amount")
-    private double soldAmount;
+    @Column(name = "quantity", nullable = false, updatable = false)
+    private double quantity;
 
     public int getId() {
         return id;
@@ -60,51 +52,75 @@ public class Transaction {
         this.createdAt = createdAt;
     }
 
-    public Account getSeller() {
-        return seller;
+    public Account getAccount() {
+        return account;
     }
 
-    public void setSeller(Account seller) {
-        this.seller = seller;
+    public void setAccount(Account account) {
+        this.account = account;
     }
 
-    public Account getBuyer() {
-        return buyer;
+    public CurrencyPair getCurrencyPair() {
+        return currencyPair;
     }
 
-    public void setBuyer(Account buyer) {
-        this.buyer = buyer;
+    public void setCurrencyPair(CurrencyPair currencyPair) {
+        this.currencyPair = currencyPair;
     }
 
-    public Currency getPurchasedCurrency() {
-        return purchasedCurrency;
+    public Operation getOperation() {
+        return operation;
     }
 
-    public void setPurchasedCurrency(Currency purchasedCurrency) {
-        this.purchasedCurrency = purchasedCurrency;
+    public void setOperation(Operation operation) {
+        this.operation = operation;
     }
 
-    public double getPurchasedAmount() {
-        return purchasedAmount;
+    public double getCourse() {
+        return course;
     }
 
-    public void setPurchasedAmount(double purchasedAmount) {
-        this.purchasedAmount = purchasedAmount;
+    public void setCourse(double course) {
+        this.course = course;
     }
 
-    public Currency getSoldCurrency() {
-        return soldCurrency;
+    public double getQuantity() {
+        return quantity;
     }
 
-    public void setSoldCurrency(Currency soldCurrency) {
-        this.soldCurrency = soldCurrency;
+    public void setQuantity(double quantity) {
+        this.quantity = quantity;
     }
 
-    public double getSoldAmount() {
-        return soldAmount;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Transaction that = (Transaction) o;
+
+        if (id != that.id) return false;
+        if (Double.compare(that.course, course) != 0) return false;
+        if (Double.compare(that.quantity, quantity) != 0) return false;
+        if (!Objects.equals(createdAt, that.createdAt)) return false;
+        if (!Objects.equals(account, that.account)) return false;
+        if (!Objects.equals(currencyPair, that.currencyPair)) return false;
+        return operation == that.operation;
     }
 
-    public void setSoldAmount(double soldAmount) {
-        this.soldAmount = soldAmount;
+    @Override
+    public int hashCode() {
+        int result;
+        long temp;
+        result = id;
+        result = 31 * result + (createdAt != null ? createdAt.hashCode() : 0);
+        result = 31 * result + (account != null ? account.hashCode() : 0);
+        result = 31 * result + (currencyPair != null ? currencyPair.hashCode() : 0);
+        result = 31 * result + (operation != null ? operation.hashCode() : 0);
+        temp = Double.doubleToLongBits(course);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        temp = Double.doubleToLongBits(quantity);
+        result = 31 * result + (int) (temp ^ (temp >>> 32));
+        return result;
     }
 }
