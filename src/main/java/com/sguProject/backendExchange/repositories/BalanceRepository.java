e4308a69a21893e12dd4b91933dcc92ad4c247e2
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-09 22:41:42 +0400

    method findAllByOwner now return Set<Balance> instead of List<Balance>

--- src/main/java/com/sguProject/backendExchange/repositories/BalanceRepository.java
+++ src/main/java/com/sguProject/backendExchange/repositories/BalanceRepository.java
@@ -5,11 +5,11 @@ import com.sguProject.backendExchange.models.Balance;
 import com.sguProject.backendExchange.models.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.Set;
 
 public interface BalanceRepository extends JpaRepository<Balance, Integer> {
-    List<Balance> findAllByOwner(Account owner);
+    Set<Balance> findAllByOwner(Account owner);
 
     Optional<Balance> findByOwnerAndCurrency(Account owner, Currency currency);
 }
