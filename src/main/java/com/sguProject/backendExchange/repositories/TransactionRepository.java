e00714b70ba1b4dd71a015b67f66f86726d27184
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-09 22:42:49 +0400

    added new method findAllByAccountId deleted method findAllBySellerIdOrBuyerId

--- src/main/java/com/sguProject/backendExchange/repositories/TransactionRepository.java
+++ src/main/java/com/sguProject/backendExchange/repositories/TransactionRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer> {
-    List<Transaction> findAllBySellerIdOrBuyerId(int seller, int buyer);
+    List<Transaction> findAllByAccountId(int accountId);
 }
