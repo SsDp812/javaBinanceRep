1e484e39c3caa5e908425a3507066fd13c09ee1e
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-11 23:06:25 +0400

    Added: create initial balance on register account

--- src/main/java/com/sguProject/backendExchange/services/AccountServiceImpl.java
+++ src/main/java/com/sguProject/backendExchange/services/AccountServiceImpl.java
@@ -1,16 +1,19 @@
 package com.sguProject.backendExchange.services;
 
 import com.sguProject.backendExchange.models.Account;
-import com.sguProject.backendExchange.models.Currency;
+import com.sguProject.backendExchange.models.Balance;
 import com.sguProject.backendExchange.repositories.AccountRepository;
 import com.sguProject.backendExchange.security.AccountDetails;
 import com.sguProject.backendExchange.services.interfaces.AccountService;
+import com.sguProject.backendExchange.services.interfaces.BalanceService;
+import com.sguProject.backendExchange.services.interfaces.CurrencyService;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -18,14 +21,25 @@ import java.util.Optional;
 @Service
 public class AccountServiceImpl implements AccountService, UserDetailsService {
 
-    private final AccountRepository accountRepository;
+    private static final String INITIAL_BALANCE_TICKER = "USDT";
+    private static final double INITIAL_BALANCE_AMOUNT = 30_000;
 
-    public AccountServiceImpl(AccountRepository accountRepository) {
+    private final AccountRepository accountRepository;
+    private final CurrencyService currencyService;
+
+    public AccountServiceImpl(AccountRepository accountRepository, CurrencyService currencyService) {
         this.accountRepository = accountRepository;
+        this.currencyService = currencyService;
     }
 
+    @Transactional
     @Override
     public Account register(Account account) {
+        Balance accountBalance = new Balance();
+        accountBalance.setCurrency(currencyService.getByTicker(INITIAL_BALANCE_TICKER));
+        accountBalance.setAmount(INITIAL_BALANCE_AMOUNT);
+        accountBalance.setOwner(account);
+
         return accountRepository.save(account);
     }
 
