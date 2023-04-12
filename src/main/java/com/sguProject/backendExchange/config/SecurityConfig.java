0ee3e68ab5a755983bf931da119aeffb5d1d8777
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-12 15:48:25 +0400

    Added PasswordEncoder BCrypt

--- src/main/java/com/sguProject/backendExchange/config/SecurityConfig.java
+++ src/main/java/com/sguProject/backendExchange/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.sguProject.backendExchange.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -29,6 +30,6 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 }
