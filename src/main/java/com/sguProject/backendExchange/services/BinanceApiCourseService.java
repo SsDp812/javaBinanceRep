f46306ec6ec45e7b1b0079e2fa87b28fb69a3ed9
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-21 19:06:52 +0400

    Added CourseNotFoundException

--- src/main/java/com/sguProject/backendExchange/services/BinanceApiCourseService.java
+++ src/main/java/com/sguProject/backendExchange/services/BinanceApiCourseService.java
@@ -4,8 +4,13 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.sguProject.backendExchange.models.CurrencyPair;
 import com.sguProject.backendExchange.services.interfaces.CourseService;
+import com.sguProject.backendExchange.util.exception.CourseNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.ResourceAccessException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Objects;
 
 @Service
 public class BinanceApiCourseService implements CourseService {
@@ -16,8 +21,18 @@ public class BinanceApiCourseService implements CourseService {
         final String url = BASE_URL + currencyPair.getBase().getTicker() + currencyPair.getQuoted().getTicker();
 
         RestTemplate restTemplate = new RestTemplate();
+        String responseString;
 
-        JsonObject response = JsonParser.parseString(restTemplate.getForObject(url, String.class)).getAsJsonObject();
+        try {
+            responseString = restTemplate.getForObject(url, String.class);
+        } catch (HttpClientErrorException e) {
+            throw new CourseNotFoundException("Invalid currency pair: " + currencyPair, e);
+        }
+        catch (ResourceAccessException e) {
+            throw new CourseNotFoundException("No connection to API", e);
+        }
+
+        JsonObject response = JsonParser.parseString(Objects.requireNonNull(responseString)).getAsJsonObject();
 
         return response.get("price").getAsDouble();
     }
