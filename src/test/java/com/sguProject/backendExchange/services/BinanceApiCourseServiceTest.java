68702f4773cb079d18bcf78feef69f2fbdd216c0
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-04-21 19:09:03 +0400

    Added getCourse_whenUsdtBtc_thenThrowException test

--- src/test/java/com/sguProject/backendExchange/services/BinanceApiCourseServiceTest.java
+++ src/test/java/com/sguProject/backendExchange/services/BinanceApiCourseServiceTest.java
@@ -1,34 +1,32 @@
 package com.sguProject.backendExchange.services;
 
+import com.sguProject.backendExchange.models.Currency;
 import com.sguProject.backendExchange.models.CurrencyPair;
-import com.sguProject.backendExchange.services.interfaces.CurrencyPairService;
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import com.sguProject.backendExchange.util.exception.CourseNotFoundException;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-class BinanceApiCourseServiceTest {
-    private final BinanceApiCourseService binanceApiCourseService;
-    private final CurrencyPairService currencyPairService;
+public class BinanceApiCourseServiceTest {
+    private static BinanceApiCourseService binanceApiCourseService;
 
-    @Autowired
-    BinanceApiCourseServiceTest(BinanceApiCourseService binanceApiCourseService,
-                                CurrencyPairService currencyPairService) {
-        this.binanceApiCourseService = binanceApiCourseService;
-        this.currencyPairService = currencyPairService;
+    @BeforeClass
+    public static void initialize() {
+        binanceApiCourseService = new BinanceApiCourseService();
     }
 
     @Test
-    void getCourse_BTCUSDT_GreaterThan0() {
-        CurrencyPair currencyPair = currencyPairService.getByBaseAndQuoted("BTC", "USDT");
+    public void getCourse_whenBtcUsdt_thenGreaterThan0() {
+        CurrencyPair currencyPair = new CurrencyPair(new Currency("BTC"), new Currency("USDT"));
         double course = binanceApiCourseService.getCourse(currencyPair);
 
-        System.out.println(course);
         assertTrue(course > 0);
+    }
+
+    @Test(expected = CourseNotFoundException.class)
+    public void getCourse_whenUsdtBtc_thenThrowException() {
+        CurrencyPair currencyPair = new CurrencyPair(new Currency("USDT"), new Currency("BTC"));
+        double course = binanceApiCourseService.getCourse(currencyPair);
     }
 }
