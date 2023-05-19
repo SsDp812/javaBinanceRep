d82b1672032bf7e5148a842b557e3fac75fd80dd
Ilya Shchelkin <ilyashchelkin@mail.ru>
2023-05-19 17:06:26 +0400

    Fix delete

--- src/main/java/com/sguProject/backendExchange/repositories/BalanceRepository.java
+++ src/main/java/com/sguProject/backendExchange/repositories/BalanceRepository.java
@@ -4,10 +4,12 @@ import com.sguProject.backendExchange.models.Account;
 import com.sguProject.backendExchange.models.Balance;
 import com.sguProject.backendExchange.models.Currency;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.Set;
 
+@Repository
 public interface BalanceRepository extends JpaRepository<Balance, Integer> {
     Set<Balance> findAllByOwner(Account owner);
 
--- src/main/java/com/sguProject/backendExchange/repositories/LimitOrderRepository.java
+++ src/main/java/com/sguProject/backendExchange/repositories/LimitOrderRepository.java
@@ -4,9 +4,11 @@ import com.sguProject.backendExchange.models.Account;
 import com.sguProject.backendExchange.models.LimitOrder;
 import com.sguProject.backendExchange.util.enums.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface LimitOrderRepository extends JpaRepository<LimitOrder, Integer> {
     List<LimitOrder> findAllByOwner(Account owner);
 
